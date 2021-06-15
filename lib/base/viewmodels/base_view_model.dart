73b0c2a1eb578240fc0d2fe85b27747e78f46c91
Burak Akten <burak_akten@hotmail.com>
2021-06-15 16:01:46 +0300

    Small Refactoring

--- lib/base/viewmodels/base_view_model.dart
+++ lib/base/viewmodels/base_view_model.dart
@@ -4,40 +4,25 @@ import 'package:flutter/foundation.dart';
 abstract class BaseViewModel extends ChangeNotifier {
   bool _isLoading = false;
   bool _isDisposed = false;
-  bool _isInitialized = false;
+  bool _isInitializeDone = false;
 
   FutureOr<void> _initState;
 
   BaseViewModel() {
-    load();
+    _init();
   }
 
   FutureOr<void> init();
 
-  void load() async {
+  void _init() async {
     this.isLoading = true;
     _initState = init();
     await _initState;
-    this._isInitialized = true;
+    this._isInitializeDone = true;
     this.isLoading = false;
   }
 
   void changeStatus() => isLoading = !isLoading;
-
-  //Getters
-  FutureOr<Function> get initState => _initState;
-
-  bool get isLoading => _isLoading;
-  bool get isDisposed => _isDisposed;
-  bool get isInitialized => _isInitialized;
-
-  //Setters
-  set isLoading(bool value) {
-    _isLoading = value;
-    scheduleMicrotask(() {
-      if (!_isDisposed) notifyListeners();
-    });
-  }
 
   void reloadState() {
     if (!isLoading) notifyListeners();
@@ -47,5 +32,20 @@ abstract class BaseViewModel extends ChangeNotifier {
   void dispose() {
     _isDisposed = true;
     super.dispose();
+  }
+
+  //Getters
+  FutureOr<Function> get initState => _initState;
+
+  bool get isLoading => _isLoading;
+  bool get isDisposed => _isDisposed;
+  bool get isInitialized => _isInitializeDone;
+
+  //Setters
+  set isLoading(bool value) {
+    _isLoading = value;
+    scheduleMicrotask(() {
+      if (!_isDisposed) notifyListeners();
+    });
   }
 }
